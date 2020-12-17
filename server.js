07aab4fac8c32debf9cd254d1de0e7b13ca921c3
Jay Peek <73311799+ChefJayPeek@users.noreply.github.com>
2020-12-17 16:02:35 -0600

    Table initalization now working.  Make the local database with the included db/schema.sql, then it should work fine for adding a user.

--- server.js
+++ server.js
@@ -6,8 +6,8 @@
 
     // Setting up port and requiring models for syncing
     const PORT = process.env.PORT || 8080;
-    const db = require("./models");
-
+    var db = require("./models");
+    // console.log(db);
     // Creating express app and configuring middleware needed for authentication
     const app = express();
     app.use(express.urlencoded({ extended: true }));
@@ -25,7 +25,7 @@
     require("./routes/api-routes.js")(app);
 
     // Syncing our database and logging a message to the user upon success
-    db.sequelize.sync().then(() => {
+    db.sequelize.sync({ force: true }).then(() => {
     app.listen(PORT, () => {
         console.log(
         "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
